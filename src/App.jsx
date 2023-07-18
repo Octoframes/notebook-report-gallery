2b5b32cab80e577557c6695be275099df63cb32b
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2023-07-18 10:39:35 +0200

    custom

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,35 @@ import { Tldraw } from '@tldraw/tldraw'
 import '@tldraw/tldraw/tldraw.css'
 
 export default function App() {
-	return (
-		<div style={{ position: 'fixed', inset: 0 }} >
-			<Tldraw renderDebugMenuItems={false}/>
-		</div>
-	)
-}
+  const handleMount = (app) => {
+    // You can use the app API here! e.g. app.selectAll()
+    // app.patchAssets({
+    //   myAssetId: {
+    //     id: "myAssetId",
+    //     type: TDAssetType.Image,
+    //     fileName: "card-repo.png",
+    //     src: "https://raw.githubusercontent.com/scikit-image/scikit-image/main/skimage/data/chelsea.png"
+    //   }
+    // });
+
+    // app.createShapes({
+    //   id: "myImage",
+    //   type: TDShapeType.Image,
+    //   assetId: "myAssetId",
+    //   point: [64, 64],
+    //   size: [400, 340]
+    // });
+  };
+
+return (
+        <div
+        style={{
+            position: "relative",
+            width: "800px",
+            height: "350px"
+        }}
+        >
+        <Tldraw onMount={handleMount} />
+        </div>
+    )
+    }
