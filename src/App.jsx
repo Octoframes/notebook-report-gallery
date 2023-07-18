df85b3df21cd412970eeb274b1e5bb4eed6c9b30
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2023-07-18 22:09:38 +0200

    reposition

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,94 @@
+import React, { useCallback } from "react";
+import { useDropzone } from "react-dropzone";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { AssetRecordType, Tldraw } from "@tldraw/tldraw";
 import "@tldraw/tldraw/tldraw.css";
+import { Base64 } from "js-base64";
 
-export default function App() {
-  const handleMount = (app) => {
-    const assetId = AssetRecordType.createId();
-    const placeholderAsset = {
-      id: assetId,
-      typeName: "asset",
-      type: "image",
-      props: {
-        w: 400,
-        h: 340,
-        name: "card-repo.png",
-        isAnimated: false,
-        mimeType: null,
-        src: "https://raw.githubusercontent.com/scikit-image/scikit-image/main/skimage/data/chelsea.png",
-      },
-      meta: {},
-    };
-
-    app.createAssets([placeholderAsset]);
-    app.createShapes([
-      {
+function App() {
+  const handleMount = useCallback((app, imageSrcs) => {
+    imageSrcs.forEach((src, index) => {
+      const assetId = AssetRecordType.createId();
+      const asset = {
+        id: assetId,
+        typeName: "asset",
         type: "image",
         props: {
           w: 400,
           h: 340,
-          assetId,
+          name: `image-${index}.png`,
+          isAnimated: false,
+          mimeType: "image/png",
+          src: src,
         },
-      },
-    ]);
-  };
+        meta: {},
+      };
+
+      app.createAssets([asset]);
+      app.createShapes([
+        {
+          type: "image",
+          props: { 
+            w: 400,
+            h: 340,
+            assetId,
+          },
+        },
+      ]);
+    });
+  }, []);
+
+  const onDrop = useCallback((acceptedFiles) => {
+    acceptedFiles.forEach((file) => {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        try {
+          const notebook = JSON.parse(e.target.result);
+          const imageSrcs = notebook.cells
+            .flatMap((cell) =>
+              cell.outputs
+                ? cell.outputs.flatMap((output) =>
+                    output.data && output.data["image/png"]
+                      ? [
+                          "data:image/png;base64," + output.data["image/png"],
+                        ]
+                      : []
+                  )
+                : []
+            )
+            .filter(Boolean);
+          handleMount(window.app, imageSrcs);
+        } catch (error) {
+          toast.error("Error parsing Jupyter notebook: " + error);
+        }
+      };
+      reader.readAsText(file);
+    });
+  }, []);
+
+  const { getRootProps, getInputProps } = useDropzone({ onDrop });
 
   return (
-    <div
-      style={{
-        position: "relative",
-        width: "1000px",
-        height: "1000px",
-      }}
-    >
-      <Tldraw onMount={handleMount} />
+    <div {...getRootProps()}>
+      <input {...getInputProps()} />
+      <ToastContainer />
+      <p>Drag 'n' drop a Jupyter notebook here, or click to select one</p>
+      <div
+        style={{
+          position: "relative",
+          width: "1000px",
+          height: "1000px",
+        }}
+      >
+        <Tldraw
+          onMount={(app) => {
+            window.app = app; // save a reference to the Tldraw instance for later
+          }}
+        />
+      </div>
     </div>
   );
 }
+
+export default App;
